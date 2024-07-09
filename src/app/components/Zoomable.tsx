07747b9018a6c015931e15fc615921aa781d13a0
KD-ayoub <ayoubkadi285@gmail.com>
2024-07-09 15:34:34 +0100

    done with zoom and mouseMove

--- src/app/components/Zoomable.tsx
+++ src/app/components/Zoomable.tsx
@@ -1,35 +1,92 @@
-import React, { MouseEvent, PropsWithChildren, useRef } from "react";
+import React, {
+  MouseEvent,
+  PropsWithChildren,
+  WheelEvent,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
 
 export default function Zoomable({ children }: PropsWithChildren) {
   const divRef = useRef<HTMLDivElement | null>(null);
-  const parentRef = useRef<HTMLDivElement | null>(null);
-  function handlMouseEnter(e: MouseEvent) {
-    // console.log(e);
-    if (divRef.current && parentRef.current) {
-      divRef.current.className = "";
-      const { x: offsetX, y: offsetY } =
-        parentRef.current.getBoundingClientRect();
-      divRef.current.style.transform = `translate(${e.clientX - offsetX}px, ${
-        e.clientY - offsetY
-      }px) scale(2.5)`;
-      //   console.log("result");
-    }
-  }
+  const childDivRef = useRef<HTMLDivElement | null>(null);
+  const [scale, setScale] = useState(1);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [prevPosition, setPrevPosition] = useState({ x: 0, y: 0 });
+  const [isDragging, setIsDragging] = useState(false);
+
+  const handlWheel = useCallback(
+    (e: WheelEvent) => {
+      if (childDivRef.current && divRef.current) {
+        const { left, top } = divRef.current.getBoundingClientRect();
+        const pointerX = e.pageX - left;
+        const pointerY = e.pageY - top;
+        const targetX = (pointerX - position.x) / scale;
+        const targetY = (pointerY - position.y) / scale;
+        let newScale =
+          scale + -1 * Math.max(-1, Math.min(1, e.deltaY)) * 0.1 * scale;
+        newScale = Math.max(0.01, Math.min(100, newScale));
+        const newPosX = -targetX * newScale + pointerX;
+        const newPosY = -targetY * newScale + pointerY;
+        console.log("dim: ", left, top, e.deltaMode);
+        setScale(newScale);
+        setPosition({ x: newPosX, y: newPosY });
+        childDivRef.current.style.transform = `translate(${newPosX}px, ${newPosY}px) scale(${newScale})`;
+      }
+    },
+    [scale, position]
+  );
+  const handlMouseDown = useCallback(
+    (e: MouseEvent) => {
+      setIsDragging(true);
+      setPrevPosition({ x: e.clientX, y: e.clientY });
+    },
+    [isDragging, prevPosition]
+  );
+
+  const handlMouseMove = useCallback(
+    (e: MouseEvent) => {
+      if (!isDragging || !childDivRef.current) return;
+      const deltaX = e.clientX - prevPosition.x;
+      const deltaY = e.clientY - prevPosition.y;
+      setPrevPosition({ x: e.clientX, y: e.clientY });
+      console.log("hererererererere....", position);
+      setPosition((prev) => ({
+        x: prev.x + deltaX,
+        y: prev.y + deltaY,
+      }));
+      childDivRef.current.style.transform = `translate(${position.x}px, ${position.y}px) scale(${scale})`;
+    },
+    [prevPosition, position]
+  );
+  const handlMouseUp = useCallback(() => {
+    setIsDragging(false);
+  }, [isDragging]);
+  /// background
+  //   style={{
+  //     backgroundImage:
+  //       "linear-gradient(#E4E4E4 1.5px, transparent 1.5px),linear-gradient(90deg, #E4E4E4 1.5px, transparent 1.5px),linear-gradient(#E4E4E4 1px, transparent 1px),linear-gradient(90deg, #E4E4E4 1px, transparent 1px);",
+  //     backgroundPosition: "-1.5px -1.5px,-1.5px -1.5px,-1px -1px,-1px -1px;",
+  //     backgroundSize: "100px 100px,100px 100px,20px 20px,20px 20px;"
+  // }}
+
   return (
     <div
-      ref={parentRef}
+      ref={divRef}
       className="absolute w-full h-full"
-      style={{
-        backgroundImage:
-          "linear-gradient(#000 1.5px, transparent 1.5px),linear-gradient(90deg, #000 1.5px, transparent 1.5px),linear-gradient(#000 1px, transparent 1px),linear-gradient(90deg, #000 1px, transparent 1px);",
-        backgroundPosition: "-1.5px -1.5px,-1.5px -1.5px,-1px -1px,-1px -1px;",
-        backgroundSize: "100px 100px,100px 100px,20px 20px,20px 20px;"
-    }}
-      onMouseMove={handlMouseEnter}
+      onWheel={handlWheel}
+      onMouseDown={handlMouseDown}
+      onMouseMove={handlMouseMove}
+      onMouseUp={handlMouseUp}
     >
       <div
-        ref={divRef}
-        style={{ width: "fit-content", transformOrigin: "0% 0%" }}
+        ref={childDivRef}
+        style={{
+          width: "fit-content",
+          transformOrigin: "0% 0%",
+          // transform: `scale(${scale}) translate(${position.x}px, ${position.y}px)`,
+        }}
       >
         {children}
       </div>
